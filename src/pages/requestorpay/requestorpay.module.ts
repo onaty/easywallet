4911a36cc020c969ed09e993ad4d223c320aa6e0
Oluwatobi Onasanya <onatty98@gmail.com>
2019-03-07 21:37:30 +0100

    create new pages and style footer

---
+++ src/pages/requestorpay/requestorpay.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { RequestorpayPage } from './requestorpay';
+
+@NgModule({
+  declarations: [
+    RequestorpayPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(RequestorpayPage),
+  ],
+})
+export class RequestorpayPageModule {}
---
+++ src/pages/searchusertopay/searchusertopay.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { SearchusertopayPage } from './searchusertopay';
+
+@NgModule({
+  declarations: [
+    SearchusertopayPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(SearchusertopayPage),
+  ],
+})
+export class SearchusertopayPageModule {}
