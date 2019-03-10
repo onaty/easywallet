e90dcb61b831559cb4f126f44a6a7e1cca032c29
Oluwatobi Onasanya <onatty98@gmail.com>
2019-03-11 00:29:56 +0100

    transactions page

--- src/+private/pages/transactions/transactions.ts
+++ src/+private/pages/transactions/transactions.ts
@@ -7,12 +7,18 @@ import {  NavController, NavParams } from 'ionic-angular';
   templateUrl: 'transactions.html',
 })
 export class TransactionsPage {
-
+  periodtype="all";searchperiod="today"
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad TransactionsPage');
   }
+  period(event){
+    console.log(this.periodtype)
+  }
+  searchperiods($event){
 
+    console.log(this.searchperiod)
+  }
 }
