04200062f2d887ee1b948042aa41cb2bbf5c4817
Oluwatobi Onasanya <onatty98@gmail.com>
2019-03-11 13:50:44 +0100

    single transaction

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,7 +10,7 @@ import { PrivatetabPage } from '../+private/pages/privatetab/privatetab';
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = PrivatetabPage;
+  rootPage:any = SigninPage;
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
